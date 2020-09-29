cb00b406acb8eba3283094bab11ddeadc29d5c54
DamieFC <69485115+DamieFC@users.noreply.github.com>
2020-09-29 16:19:54 -0700

    Add files via upload
    New files for filesystem and apps

---
+++ apps for Operating System.sh
@@ -0,0 +1,22 @@
+#apps for Operating System
+
+sudo apt-get nmap
+mv nmap /home/apps/hackerstuff
+sudo apt-get john
+mv john /home/apps/hackerstuff
+sudo apt-get #etc, except with app names
+sudo apt-get #probably a GUI thing             __
+sudo apt-get #oh boy, I hope you get the idea (: )
+sudo apt-get leafpad
+sudo apt-get hashcat
+mv hascat /home/apps/hackerstuff
+sudo apt-get sqlmap
+mv sqlmap /home/apps/hackerstuff
+sudo apt-get #so on and so on
+sudo apt-get #so on and so on
+sudo apt-get #so on and so on
+sudo apt-get #so on and so on
+sudo apt-get #so on and so on
+sudo apt-get #so on and so on
+sudo apt-get #so on and so on
+sudo apt-get #so on and so on
---
+++ filesystem.sh
@@ -0,0 +1,16 @@
+#filesystem
+
+sudo mkdir /                            #root directory
+sudo mkdir /home                        #where user stuff goes
+sudo mkdir /var                         #important variables
+sudo mkdir /boot                        #boot up files
+sudo mkdir /home/apps                   #applications go here
+sudo mkdir /home/apps/hackerstuff       #hacker stuff, e.g nmap
+sudo mkdir /home/apps/productivity      #productivity apps
+sudo mkdir /bin                         #bin for libraries
+sudo mkdir /sbin                        #more libraries
+sudo mkdir /sbin/bin                    #more libraries
+sudo mkdir /home/userstuff              #user stuff
+sudo mkdir /home/userstuff/media        #user media
+sudo mkdir /home/userstuff/extern       #external storage devices
+sudo mkdir /home/userstuff/extra        #other user stuff
