b1062bff207071219f677bf1194542fb9d9af29c
DamieFC <69485115+DamieFC@users.noreply.github.com>
2020-10-01 10:05:55 -0700

    Update filesystem.sh
    added root user world, extra files

--- filesystem.sh
+++ filesystem.sh
@@ -1,4 +1,5 @@
-#filesystem
+#filesystem for OS
+#currently, it doesn't support more than 2 users, for the whole OS
 
 sudo mkdir /                            #root directory
 sudo mkdir /home                        #where user stuff goes
@@ -10,7 +11,14 @@ sudo mkdir /home/apps/productivity      #productivity apps
 sudo mkdir /bin                         #bin for libraries
 sudo mkdir /sbin                        #more libraries
 sudo mkdir /sbin/bin                    #more libraries
-sudo mkdir /home/userstuff              #user stuff
-sudo mkdir /home/userstuff/media        #user media
-sudo mkdir /home/userstuff/extern       #external storage devices
-sudo mkdir /home/userstuff/extra        #other user stuff
+sudo mkdir /home/userland               #user stuff
+sudo mkdir /home/userland/media         #user media
+sudo mkdir /home/userland/extern        #external storage devices
+sudo mkdir /home/userland/extra         #other user stuff
+sudo mkdir /root/apps                   #root user applications go here
+sudo mkdir /root/apps/hackerstuff       #roothacker stuff, e.g nmap
+sudo mkdir /root/apps/productivity      #root productivity apps
+sudo mkdir /root/userland               #user stuff
+sudo mkdir /root/userland/media         #root user media
+sudo mkdir /root/userland/extern        #root external storage devices
+sudo mkdir /root/userland/extra         #other root user stuff
